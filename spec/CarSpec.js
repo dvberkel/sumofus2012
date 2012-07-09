c8d803ba5feca835827284020f50d050c0e10e29
jorik <jorik@jorik-desktop.(none)>
2012-07-09 23:10:50 +0200

    Added tests for cars. Added a Model for a car. Added tests for nodes.

--- spec/CarSpec.js
+++ spec/CarSpec.js
@@ -2,4 +2,36 @@ describe("a Car", function(){
     it("should be defined in the SumOfUs namespace", function(){
 	expect(SumOfUs.Car).toBeDefined();
     });
+
+    it("should have correct defaults", function(){
+        var car = new SumOfUs.Car;
+	expect(car).toHaveSpeed(0);
+	expect(car).toHaveMaxSpeed(5);
+	expect(car).toHaveAcceleration(1);
+        
+        var pos = car.get("position");
+	var dir = car.get("direction");
+	expect(pos).toBeUndefined();
+	expect(dir).toBeUndefined();
+
+	expect(car).toBeHighlighted(false);
+    });
+
+    it("should be highlightable", function(){
+        var car = new SumOfUs.Car;
+	car.changeHighlight(true);
+	expect(car).toBeHighlighted(true);
+	car.changeHighlight(false);
+	expect(car).toBeHighlighted(false);
+    });
+
+    it("should be movable", function(){
+        var car = new SumOfUs.Car;
+	var node = new SumOfUs.TrackNode({directions : ["up","down"]});
+	car.moveTo(node,"up",2);
+	expect(car).toBeAt(node);
+	expect(car).toHaveSpeed(2);
+	expect(car).toBeGoingInDirection("up");
+    });
+
 });
--- src/Car.js
+++ src/Car.js
@@ -1,5 +1,25 @@
 (function(_, Backbone, SumOfUs, undefined){
-    var Car = Backbone.Model.extend({});
+    var Car = Backbone.Model.extend({
+        defaults : {
+	    speed : 0,
+	    maxSpeed : 5,
+	    acceleration : 1,
+	    position : undefined,
+	    direction : undefined,
+
+	    highlighted  : false
+	},
+
+	moveTo : function(position, direction, speed){
+	    this.set("position", position);
+	    this.set("direction", direction);
+	    this.set("speed", speed);
+	},
+
+        changeHighlight : function(setting){
+	   this.set("highlighted", setting);
+	}
+    });
 
     SumOfUs.Car = Car;
 })(_, Backbone, SumOfUs);
